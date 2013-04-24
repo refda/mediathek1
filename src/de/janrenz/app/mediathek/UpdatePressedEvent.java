3c8ac96141a22ceefa98517697eaaa44da897f77
Jan Renz <janrenz@Jans-MacBook-Pro.local>
2013-04-24 09:20:33 +0200

    + new files

---
+++ src/de/janrenz/app/mediathek/UpdatePressedEvent.java
@@ -0,0 +1,9 @@
+package de.janrenz.app.mediathek;
+public class UpdatePressedEvent {
+  public UpdatePressedEvent( ) {
+   
+  }
+  @Override public String toString() {
+    return "UpdatePressed";
+  }
+}
